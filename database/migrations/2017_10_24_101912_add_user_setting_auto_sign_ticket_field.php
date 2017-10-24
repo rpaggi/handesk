e8059f9cd2ac3ae9eae6b10ccff4d76e2b094db4
Ronaldo Paggi <ronaldo.paggi@gmail.com>
2017-10-24 08:28:15 -0200

    Create migration file to add field auto_sign_ticket at user_settings table

---
+++ database/migrations/2017_10_24_101912_add_user_setting_auto_sign_ticket_field.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserSettingAutoSignTicketField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_settings', function($table){
+            $table->boolean('auto_sign_ticket')
+              ->nullable()->after('lead_assigned_notification');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_settings', function($table){
+            $table->dropColumn('auto_sign_ticket');
+        });
+    }
+}
